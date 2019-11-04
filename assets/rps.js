d44e6ad70396f3e4f3fe803cb1a3c71c0cb3f626
noeljusw14 <matthewhnoel@gmail.com>
2019-11-04 18:33:05 -0500

    homework not done

--- assets/rps.js
+++ assets/rps.js
@@ -13,109 +13,114 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database()
 database.ref().set({
-    message: 'hello world!'
+    message: 'hello world!',
+    message: 'forever'
 })
 //set up variable array
 var trainSchedule = [];
+var trainName = "";
+var destination = "";
 
+var arrival = "0";
+
+var frequency = "";
 
 //set up functions
 function addTrain() { }
 function getTrain() { }
 function renderData() { }
 // Code to calculate when the next train will arrive; this should be relative to the current time
-function nextTrain() { }
 
-function submitBtn() {
-    $('#submit').on('click', function (event) {
-        event.preventDefault();
 
-//set input value as variables
-        var trainName = $('#trainInput').val().trim()
+$('#submit').on('click', function (event) {
+    event.preventDefault();
 
-        var destination = $('#destInput').val().trim()
+    //set input value as variables
+    trainName = $('#trainInput')
+    .val()
+    .trim();
 
-        var arrival = $('#arrivalInput').val().trim()
+    destination = $('#destInput')
+    .val()
+    .trim();
 
-        var frequency = $('#freqInput').val().trim()
+  //arrival = moment($('#arrivalInput')).val().trim();
 
-        var newTrain = {
-            name: trainName,
-            dest: destination,
-            arrive: arrival,
-            freq: frequency,
-        }
+    frequency = $('#freqInput')
+    .val()
+    .trim();
 
-        if (trainName, destination, arrival, frequency == '') {
-            return false
-        }
-
-    })
     //push info to firebase database
-    database.ref().push(newTrain)
+    database.ref().push({
+        name: trainName,
+        destination: destination,
+        frequency: frequency, 
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    })
 
     console.log(newTrain.name)
     console.log(newTrain.dest)
-    console.log(newTrain.arrive)
     console.log(newTrain.freq)
 
-//alert notificaton once train is added to table/database
+    //alert notificaton once train is added to table/database
     alert('New Train Schedule successfully added!');
 
     $('#trainInput').val("");
     $('#destInput').val("");
-    $('#arrivalInput').val("");
     $('#freqInput').val("");
-}
 
- //get info back from firebase to populate page
-database.ref().on('child_added', function (childSnapshot) {
-    console.log(childSnapshot)
+})
 
-    var trainName = childSnapshot.val().name;
 
-    var destination = childSnapshot.val().dest;
 
-    var arrival = childSnapshot.val().arrive;
+//get info back from firebase to populate page
+database.ref().on('child_added', function (snapshot) {
 
-    var frequency = childSnapshot.val().freq;
+    console.log(snapshot)
+
+    trainName = snapshot.val().name;
+
+    destination = snapshot.val().dest;
+
+    frequency = snapshot.val().freq;
 
 
     console.log(trainName)
     console.log(destination)
-    console.log(arrival)
     console.log(frequency)
-    
-//creating a new row dynamically
+
+    //  function nextTrain() { };
+    //  function minutesAway(){};
+
+    //creating a new row dynamically
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
-        $("<td>").text(arrival),
         $("<td>").text(frequency),
-      );
-    
-//adding the new row to the table body
-      $('#train-schedule > tbody').append(newRow);
-    
+    );
+
+    //adding the new row to the table body
+    $('#train-schedule').append("");
+
 })
 
 // Try adding update and remove buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
-function removeBtn() {
-    $('#remove').on('click', function () {
+// function removeBtn() {
+//     $('#remove').on('click', function () {
 
-    })
-}
-//use firebase to set up email sign in
-function signIn() {
-    
-}
+//     })
+// }
+// //use firebase to set up email sign in
+// function signIn() {
 
-    
+// }
+
+
 
     //create timer function and format
 
     //create .on(value) function/method to get update firebase
 
-   
+
 
 
